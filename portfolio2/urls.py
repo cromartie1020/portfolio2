114631ed9e9434da84fed1dce81718bdcd52fdb4
cromartie1020 <hrc245@gmail.com>
2021-03-17 22:16:32 -0400

    my first commit

---
+++ portfolio2/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from user import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('login/',auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    #path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    #path('register/',views.register,name='register'),
+    path('user/',include('user.urls')),
+]
